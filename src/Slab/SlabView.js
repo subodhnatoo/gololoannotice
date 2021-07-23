bbbd979fececad671824be9769d5aaf8fad773d3
Subodh <Subodh.natoo@kairee.in>
2021-07-23 15:04:04 +0530

    Test

--- src/Slab/SlabView.js
+++ src/Slab/SlabView.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const SlabView = () => {
     return (
         <div>
-            <h1>This is Slab View</h1>
+            <h1>This is Slab View from Master</h1>
         </div>
     )
 }
